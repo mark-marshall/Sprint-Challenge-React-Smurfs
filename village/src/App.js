04b254e824ab12dee7082fd5318bd27c3a02cb0e
mark-marshall <mark.marshallgp@gmail.com>
2019-03-01 08:31:52 +0000

    [MVP] Added smurfs data on state

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -9,11 +9,24 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      error: '',
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount(){
+    axios
+    .get('http://localhost:3333/smurfs')
+    .then(smurfs => this.updateSmurfs(smurfs.data))
+    .catch(error => this.updateErrors(error.message))
+  }
+
+  updateSmurfs = smurfs => {this.setState({ smurfs })}
+
+  updateErrors = error => {this.setState({ error })}
+
   render() {
     return (
       <div className="App">
